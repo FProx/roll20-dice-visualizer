b50d96e4a28a379be8c6fb77e4748d439a7b0360
FProx <17589131+FProx@users.noreply.github.com>
2024-07-04 16:44:07 +0200

    help messages

--- diceroll_analysis.py
+++ diceroll_analysis.py
@@ -9,20 +9,21 @@ parser = argparse.ArgumentParser(prog='Roll20 Dice Analyzer',
                                  description='This script takes in a chat archive from roll20.net and generates plots with dice distributions for each player.',
                                  epilog='For inquiries, please contact @FProx on GitHub: https://github.com/FProx')
 parser.add_argument('file_path', help='relative path to chat archive. Its format should be .htm')
-parser.add_argument('-d', '--dice_size', type=int)
+parser.add_argument('-d', '--dice_type', type=int, help='use this if results of a specific dice type should be explored. Please pass the side count as an integer.')
 names_group = parser.add_mutually_exclusive_group()
-names_group.add_argument('-p', '--players', nargs='+')
-names_group.add_argument('-x', '--exclude', nargs='+')
-parser.add_argument('--absolute', action='store_true', default=False)
-parser.add_argument('--pseudonymized', action='store_true', default=False)
-parser.add_argument('-s', '--save_figure', action='store', default=False)
+names_group_help_appendix = 'Separate multiple players by leaving a space. If a player name contains a space, please enclose it with ". Only exact name matches work.'
+names_group.add_argument('-p', '--players', nargs='+', help=f'display only results of one or multiple players. {names_group_help_appendix}')
+names_group.add_argument('-x', '--exclude', nargs='+', help=f'exclude one or multiple players. {names_group_help_appendix}')
+parser.add_argument('--absolute', action='store_true', default=False, help='if set, displays absolute result counts instead of probabilities (the latter is the default).')
+parser.add_argument('--pseudonymized', action='store_true', default=False, help='if set, shows pseudonomized player ids instead of the names shown in the chat window (the latter is the default).')
+parser.add_argument('-s', '--save_figure', action='store', default=False, help='if set, saves the generated figure at the specified path. By default, the generated figure will be shown to the user but not saved.')
 
 player_pattern = br'data-playerid="([^"]+)">(?:(?!<div class="message).)+'
 dice_pattern = r'diceroll d(\d+).+?didroll">(\d+)'
 name_pattern = r'<span class="by">([^:]+):</span>'
 
 def create_dataframe_from_roll20_chat(raw_text_path, only_include_players=None, exclude_players=None, is_pseudonomized=False):
-    roll_data = {'playerid': [], 'dice_size': [], 'dice_roll': []}
+    roll_data = {'playerid': [], 'dice_type': [], 'dice_roll': []}
     player_name_map = {} # maps playerid to the player name as it would be displayed in the chat log.
     
     with open(raw_text_path, 'r+') as file: # fill roll_data dict
@@ -39,7 +40,7 @@ def create_dataframe_from_roll20_chat(raw_text_path, only_include_players=None, 
             for diceroll_match in diceroll_matches:
                 size, roll = diceroll_match.groups()
                 roll_data['playerid'].append(playerid)
-                roll_data['dice_size'].append(int(size))
+                roll_data['dice_type'].append(int(size))
                 roll_data['dice_roll'].append(int(roll))
     
     roll_df = pd.DataFrame.from_dict(roll_data, orient='columns') # create df from dict and filter it
@@ -49,23 +50,23 @@ def create_dataframe_from_roll20_chat(raw_text_path, only_include_players=None, 
     elif exclude_players:
         roll_df = roll_df.drop(roll_df.loc[roll_df['display_name'].isin(exclude_players)].index)
     
-    return roll_df[['display_name', 'dice_size', 'dice_roll']]
+    return roll_df[['display_name', 'dice_type', 'dice_roll']]
 
 def normalize_dice_df(df):
     norm_df = df.copy()
-    norm_df['dice_roll'] = (norm_df['dice_roll']-1)/(norm_df['dice_size']-1)
+    norm_df['dice_roll'] = (norm_df['dice_roll']-1)/(norm_df['dice_type']-1)
     return norm_df
 
-def filter_dice_size(df, dice_size):
-    filtered_df = df.loc[df['dice_size'] == dice_size]
+def filter_dice_type(df, dice_type):
+    filtered_df = df.loc[df['dice_type'] == dice_type]
     return filtered_df
 
-def generate_plot_args(df, dice_size=None, show_count=False):
+def generate_plot_args(df, dice_type=None, show_count=False):
     total_rolls = len(df)
-    player_total_rolls = df.groupby(['display_name']).count()['dice_size']
+    player_total_rolls = df.groupby(['display_name']).count()['dice_type']
     df['display_name_and_rolls'] = df['display_name'].apply(lambda name: f'{name} ({player_total_rolls[name]} rolls)')
     
-    dice_result_dist_string = f'd{dice_size}' if dice_size else 'Dice'
+    dice_result_dist_string = f'd{dice_type}' if dice_type else 'Dice'
     dice_result_dist_string += ' Distribution'
     title = f'Absolute {dice_result_dist_string}' if show_count else f'Relative {dice_result_dist_string}'
     is_single_player = len(df['display_name'].unique()) == 1
@@ -77,15 +78,15 @@ def generate_plot_args(df, dice_size=None, show_count=False):
     figure_args = {
         'df': df,
         'title': title,
-        'xlabel': 'Dice Result' if dice_size else 'Dice Result Percentile',
-        'xticks': np.arange(1, dice_size + 1, 1) if dice_size else np.arange(0, 1.1, .1)
+        'xlabel': 'Dice Result' if dice_type else 'Dice Result Percentile',
+        'xticks': np.arange(1, dice_type + 1, 1) if dice_type else np.arange(0, 1.1, .1)
     }
     histplot_args = {
         'x': 'dice_roll',
         'hue': None if is_single_player else 'display_name_and_rolls',
         'stat': 'count' if show_count else 'probability',
-        'bins': dice_size if dice_size else 6,
-        'discrete': True if dice_size else False,
+        'bins': dice_type if dice_type else 6,
+        'discrete': True if dice_type else False,
         'common_norm': True if is_single_player else False,
         'element': 'bars' if is_single_player else 'step', # for visibility
         'fill': True if is_single_player else False
@@ -114,11 +115,11 @@ def plot_dice_results(df, title, xlabel, xticks, save_figure=False, **histplot_a
 
 def main(args):
     roll_df = create_dataframe_from_roll20_chat(args.file_path, args.players, args.exclude, args.pseudonymized)
-    if not args.dice_size:
+    if not args.dice_type:
         roll_df = normalize_dice_df(roll_df)
     else:
-        roll_df = filter_dice_size(roll_df, args.dice_size)
-    figure_args, histplot_args = generate_plot_args(roll_df, args.dice_size, args.absolute)
+        roll_df = filter_dice_type(roll_df, args.dice_type)
+    figure_args, histplot_args = generate_plot_args(roll_df, args.dice_type, args.absolute)
     plot_dice_results(**figure_args, **histplot_args, save_figure=args.save_figure)
 
 if __name__ == '__main__':
